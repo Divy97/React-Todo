f4ce04cf2ec369783c0cb100f9c2ca439d88c5b3
Divy Parekh <divyparekh1810@gmail.com>
2022-04-21 19:11:08 +0530

    Created Reducer, TodoContext and action file

---
+++ src/Context/reducer.js
@@ -0,0 +1,15 @@
+import { ADD_TODO, REMOVE_TODO } from "./action.types";
+
+export default (state, action) => {
+    switch (action.type) {
+        case ADD_TODO:
+            return[...state, action.payload];
+            break;
+        case REMOVE_TODO:
+            return stste.filter(todo => todo.id !== action.payload)
+            break;
+        default:
+            return state;
+            break;
+    }
+}
